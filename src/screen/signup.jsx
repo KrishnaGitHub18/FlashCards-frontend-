d58e2f39feb18cdd73036ee6ec72c3cbab3caafe
KrishnaGitHub18 <krishnadeheriya1803@gmail.com>
2024-10-05 13:06:12 +0530

    z

--- src/screen/signup.jsx
+++ src/screen/signup.jsx
@@ -19,8 +19,8 @@ const Login = () => {
         console.log('Email:', email);
 
         // const response = await fetch('https://flash-card-backend-ten.vercel.app/api/login', {
-        const response = await fetch('http://localhost:5000/api/signup-req', {
-        // const response = await fetch('https://flash-card-backend-ten.vercel.app/api/signup-req', {
+        // const response = await fetch('http://localhost:5000/api/signup-req', {
+        const response = await fetch('https://flash-card-backend-ten.vercel.app/api/signup-req', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
